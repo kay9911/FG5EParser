a9f01615f2f742c556ed37eb9f0f4014eca7e967
Kayne Padua <kay.padua@gmail.com>
2017-05-19 22:09:18 +0530

    Spell Implementations

--- FG5EParser_v_2.0/Pages/Player Module/Spells.xaml.cs
+++ FG5EParser_v_2.0/Pages/Player Module/Spells.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FG5eParserLib.View_Models;
+using System;
 using System.Windows.Controls;
 
 namespace FG5EParser_v_2._0.Pages.Player_Module
@@ -20,6 +21,32 @@ namespace FG5EParser_v_2._0.Pages.Player_Module
             // UI Constants/Bindings
             cmbLevels.ItemsSource = _SPV._LevelList;
             cmbSpellSchool.ItemsSource = _SPV._SpellSchools;
+        }
+
+        private void btnAddFromList_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (dtSpellNames.Visibility == System.Windows.Visibility.Hidden)
+            {
+                dtSpellNames.Visibility = System.Windows.Visibility.Visible;
+                txtOutput.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                dtSpellNames.Visibility = System.Windows.Visibility.Hidden;
+                txtOutput.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void dtSpellNames_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (dtSpellNames.SelectedItem == null)
+            {
+                // DO NOTHING
+            }
+            else
+            {
+                txtSpellList.Text = !string.IsNullOrEmpty(txtSpellList.Text) ? txtSpellList.Text + Environment.NewLine + _SPV.getSelectedSpellName(dtSpellNames.SelectedItem) : _SPV.getSelectedSpellName(dtSpellNames.SelectedItem);
+            }
         }
     }
 }
--- FG5eParserLib/View Models/SpellViewModel.cs
+++ FG5eParserLib/View Models/SpellViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using FG5eParserModels.Player_Models;
 using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using System.Text;
 
 namespace FG5eParserLib.View_Models
 {
@@ -23,17 +25,45 @@ namespace FG5eParserLib.View_Models
                 OnPropertyChanged(null);
             }
         }
+        private string _SpellListName { get; set; }
+        public string SpellListName
+        {
+            get
+            {
+                return _SpellListName;
+            }
+            set
+            {
+                _SpellListName = value;
+                OnPropertyChanged("SpellListName");
+            }
+        }
+        private string _SpellsinList { get; set; }
+        public string SpellsinList
+        {
+            get
+            {
+                return _SpellsinList;
+            }
+            set
+            {
+                _SpellsinList = value;
+                OnPropertyChanged("SpellsinList");
+            }
+        }
 
         // Properties and Lists
         public string SpellsTextPath { get; set; }
         public ObservableCollection<Spells> _spellList { get; set; }
         public List<string> _LevelList { get; set; }
         public List<string> _SpellSchools { get; set; }
+        public List<string> _SpellLists { get; set; }
 
         // Relay Commands
         public RelayCommand SaveSpells { get; set; } // Save Button
         public RelayCommand ResetFields { get; set; } // Reset Button
         public RelayCommand AddSpell { get; set; } // Add Spell
+        public RelayCommand AddSpellList { get; set; } // Spell Lists
 
         // Output
         private string _Output { get; set; }
@@ -43,21 +73,22 @@ namespace FG5eParserLib.View_Models
         public SpellViewModel()
         {
             // Class Objects Inits
-            _SpellObj = new Spells();
+            _SpellObj = new Spells() { _IsVerbal = "true" };
 
             // Lists Inits
             _spellList = new ObservableCollection<Spells>();
+            _SpellLists = new List<string>();
             _LevelList = new List<string>() {
                 "",
-                "1st Level",
-                "2nd Level",
-                "3rd Level",
-                "4th Level",
-                "5th Level",
-                "6th Level",
-                "7th Level",
-                "8th Level",
-                "9th Level",
+                "1st-Level",
+                "2nd-Level",
+                "3rd-Level",
+                "4th-Level",
+                "5th-Level",
+                "6th-Level",
+                "7th-Level",
+                "8th-Level",
+                "9th-Level",
             };
 
             _SpellSchools = new List<string>(){
@@ -68,12 +99,15 @@ namespace FG5eParserLib.View_Models
                 "Enchantment",
                 "Evocation",
                 "Illusion",
-                "Necromancy"
+                "Necromancy",
+                "Transmutation"
             };
 
             // Delegates
             SaveSpells = new RelayCommand(saveSpells, canSaveSpells);
             ResetFields = new RelayCommand(resetFields);
+            AddSpell = new RelayCommand(addSpell, canAddSpell);
+            AddSpellList = new RelayCommand(addSpellList, canAddSpellList);
         }
 
         // Functions
@@ -116,6 +150,119 @@ namespace FG5eParserLib.View_Models
         {
             // Reset the object and refresh the screen
             SpellObject = new Spells();
+        }
+
+        private void addSpell(object obj)
+        {
+            // Add the spell to the list
+            _spellList.Add(SpellObject);
+
+            getOutput();
+
+            // Reset the object and refresh the screen
+            SpellObject = new Spells() { _Level = SpellObject._Level, _School = SpellObject._School, _IsVerbal = "true" };
+        }
+
+        private bool canAddSpell(object obj)
+        {
+            // validation logic goes here
+            return true;
+        }
+
+        // Output Function
+        private void getOutput()
+        {
+            StringBuilder _sb = new StringBuilder();
+
+            if (_SpellLists.Count != 0)
+            {
+                _sb.Append("#@;");
+                _sb.Append(Environment.NewLine);
+
+                for (int i = 0; i < _SpellLists.Count; i++)
+                {
+                    // List name
+                    _sb.Append(_SpellLists[i].ToString());
+                    _sb.Append(Environment.NewLine);
+
+                    i++; // Moving to the spells
+
+                    // get the list of spells
+                    string[] lines = _SpellLists[i].Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+                    foreach (var _spell in lines)
+                    {
+                        _sb.Append(_spell);
+                        _sb.Append(Environment.NewLine);
+                    }
+                }
+            }
+
+            if (_spellList.Count != 0)
+            {
+                _sb.Append("##;");
+                _sb.Append(Environment.NewLine);
+
+                foreach (var spell in _spellList)
+                {
+                    _sb.Append(spell._Name);
+                    _sb.Append(Environment.NewLine);
+
+                    _sb.Append(string.Format("{0} {1}",spell._Level,spell._School));
+                    _sb.Append(Environment.NewLine);
+
+                    _sb.Append(string.Format("Casting Time: {0}",spell._CastingTime));
+                    _sb.Append(Environment.NewLine);
+
+                    _sb.Append(string.Format("Range: {0}", spell._Range));
+                    _sb.Append(Environment.NewLine);
+
+                    string _comp = string.Empty ;
+                    // Components
+                    if (!string.IsNullOrEmpty(spell._IsVerbal))
+                    {
+                        _comp = "V,";
+                    }
+                    if (!string.IsNullOrEmpty(spell._IsSomatic))
+                    {
+                        _comp = _comp + "S,";
+                    }
+                    if (!string.IsNullOrEmpty(spell._Material))
+                    {
+                        _comp = _comp + string.Format("M ({0})",spell._Material);
+                    }
+                    _sb.Append(string.Format("Components: {0}", _comp));
+                    _sb.Append(Environment.NewLine);
+
+                    _sb.Append(string.Format("Duration: {0}",spell._Duration));
+                    _sb.Append(Environment.NewLine);
+
+                    _sb.Append(spell._Description);
+                    _sb.Append(Environment.NewLine);
+                }
+            }
+            Output = _sb.ToString();
+        }
+
+        public string getSelectedSpellName(object obj)
+        {
+            return ((Spells)obj)._Name;
+        }
+
+        private void addSpellList(object obj)
+        {
+            _SpellLists.Add(SpellListName);
+            _SpellLists.Add(SpellsinList);
+
+            getOutput();
+
+            SpellListName = string.Empty;
+            SpellsinList = string.Empty;
+        }
+
+        private bool canAddSpellList(object obj)
+        {
+            return true;
         }
 
         #region PROPERTY CHANGES
