451eb337635ea73098fb12c24569b9ab320a44f3
Kayne Padua <kay.padua@gmail.com>
2017-08-13 12:05:14 +0530

    ClassControl.cs clean ups

--- FG5EParser_v_2.0/Controls/ClassControl.xaml.cs
+++ FG5EParser_v_2.0/Controls/ClassControl.xaml.cs
@@ -11,17 +11,5 @@ namespace FG5EParser_v_2._0.Controls
         {
             InitializeComponent();
         }
-
-        private void btnReview_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            if (txtOutput.Visibility == System.Windows.Visibility.Hidden)
-            {
-                txtOutput.Visibility = System.Windows.Visibility.Visible;
-            }
-            else
-            {
-                txtOutput.Visibility = System.Windows.Visibility.Hidden;
-            }
-        }
     }
 }
